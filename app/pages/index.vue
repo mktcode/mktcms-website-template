393f95bc9810e29196679feca193a2b28a0ec078
mktcode <kontakt@markus-kottlaender.de>
2026-01-23 00:53:51 +0100

    feat: create page component and update index page to use new structure

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,31 +1,9 @@
 <script setup lang="ts">
-const { data: content } = await useFetch('/api/content/home.md')
+const content = await useMdContent('Startseite.md')
 </script>
 
 <template>
-  <div>
-    <header class="bg-brand-soft">
-      <nav class="container max-w-292.5 mx-auto px-4 py-3 bg-brand text-white">
-        <a href="/" class="font-bold">Meine Webseite</a>
-      </nav>
-    </header>
-    <main>
-      <div class="container max-w-292.5 mx-auto p-4 bg-white">
-        <div class="bg-brand-soft rounded-sm p-4 mb-4">
-          <h1 class="text-4xl font-bold">Hallo Welt!</h1>
-        </div>
-        <nav class="bg-brand text-white text-sm rounded-sm mb-4 flex overflow-hidden">
-          <NuxtLink to="/" class="p-4 hover:bg-brand-hover">Startseite</NuxtLink>
-          <NuxtLink to="/contact" class="p-4 hover:bg-brand-hover">Kontakt</NuxtLink>
-        </nav>
-        <div v-html="content" class="prose max-w-none" />
-      </div>
-    </main>
-    <footer class="bg-brand-soft">
-      <div class="container max-w-292.5 mx-auto px-4 py-3 bg-brand text-white text-right text-sm">
-        <NuxtLink to="/impressum" class="hover:underline">Impressum</NuxtLink>
-        <NuxtLink to="/datenschutz" class="ml-4 hover:underline">Datenschutz</NuxtLink>
-      </div>
-    </footer>
-  </div>
+  <Page>
+    <div v-html="content.html" class="prose max-w-none" />
+  </Page>
 </template>
