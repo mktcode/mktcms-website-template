65066fa553bab59ebb08372c7b535abd3de59552
mktcode <kontakt@markus-kottlaender.de>
2026-01-25 00:15:01 +0100

    feat: add md component to render markdown content

---
+++ app/components/mdc/md.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { path } = defineProps<{ path: string }>()
+
+const content = await useMdContent(path + '.md')
+</script>
+
+<template>
+  <div v-html="content.html" class="prose max-w-none" />
+</template>
