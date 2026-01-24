8f5a7fa4ca14607459772af65cc190788fdb3037
mktcode <kontakt@markus-kottlaender.de>
2026-01-24 19:39:01 +0100

    refactor: remove unused CSS styles for nested dropdowns in dropdown component

--- app/components/navMobile/dropdown.vue
+++ app/components/navMobile/dropdown.vue
@@ -40,22 +40,3 @@ defineProps<{
     </div>
   </div>
 </template>
-
-<style scoped>
-/* Class to make first-level dropdown appear on the right side (aligned to right edge) */
-.dropdown.submenu-left {
-  left: auto;
-  right: 0;
-}
-
-/* Class to make nested submenus appear on the left instead of right */
-.nested-dropdown.submenu-left {
-  left: auto;
-  right: 100%;
-  transform: translateX(0.5rem); /* translate-x-2 equivalent */
-}
-
-.nested-dropdown.submenu-left:is(.group-hover\/nested *) {
-  transform: translateX(0);
-}
-</style>
